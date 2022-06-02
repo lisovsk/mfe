52ceb3e100ed7d9d9fd10fca2b6945c5848ab832
lisovsk <lisovskyi.dmytro@gmail.com>
2022-06-03 00:52:53 +0300

    added remotes for auth

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,6 +15,7 @@ const prodConf = {
 		new ModuleFederationPlugin({
 			name: 'container',
 			remotes: {
+				auth: `auth@${domain}/auth/latest/remoteEntry.js`,
 				marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
 			},
 			shared: packageJson.dependencies
